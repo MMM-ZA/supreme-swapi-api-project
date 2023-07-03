e0cf5ce81c0a4dfa1b4d649e9b47cf063c29f204
makoma mabilo <makoma44@gmail.com>
2023-07-03 21:52:42 +0800

    added styling changes, async function

--- supreme-swapi-api-project/src/App.js
+++ supreme-swapi-api-project/src/App.js
@@ -1,32 +1,41 @@
 import React from 'react';
 import { useState } from 'react';
+import StarWars from '../src/assets/StarWarsPoster.png'
 import MoviesList from './components/MoviesList';
-import './App.css';
+import classes from './App.module.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function FetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedData = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedData);
+  async function FetchMoviesHandler() {
+    try {
+      const response = await fetch('https://swapi.dev/api/films');
+      const data = await response.json();
+
+      const transformedData = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
       });
+
+      setMovies(transformedData);
+    } catch (error) {
+      // Handle error if the fetch or JSON parsing fails
+      console.error('Error fetching movies:', error);
+    }
   }
 
   return (
     <React.Fragment>
       <section>
+        <header>
+          <div className={classes['main-image']}>
+         <img src={StarWars} alt='Star wars logo'/>
+         </div>
+        </header>
         <button onClick={FetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
