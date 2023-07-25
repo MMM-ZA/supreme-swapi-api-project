2ba00c2d7e5e955c08e4f23910b3cc94422d9d11
makoma mabilo <makoma44@gmail.com>
2023-07-25 21:49:15 +0800

    added character.js

---
+++ supreme-swapi-api-project/src/components/Characters.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const CharacterList = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      try {
+        const response = await axios.get('https://swapi.dev/api/people/');
+        setCharacters(response.data.results);
+      } catch (error) {
+        console.error('Error fetching characters:', error);
+      }
+    };
+
+    fetchCharacters();
+  }, []);
+
+  return (
+    <div>
+      <h1>Star Wars Characters</h1>
+      <ul>
+        {characters.map((character) => (
+          <li key={character.name}>{character.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CharacterList;
