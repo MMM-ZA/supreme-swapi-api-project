fcc453de4c3517289a572b4e2d5e2dc27a2e7115
makoma mabilo <makoma44@gmail.com>
2023-08-12 20:51:56 +0800

    managing characters.js api request

--- supreme-swapi-api-project/src/App.js
+++ supreme-swapi-api-project/src/App.js
@@ -1,28 +1,24 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Films from './pages/Films'
-import Characters from './pages/Characters'
-import Starships from './pages/Starships'
-import Vehicles from './pages/Vehicles'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';  // Import Routes from react-router-dom
+import Films from './pages/Films';
+import Characters from './pages/Characters';
+import Starships from './pages/Starships';
+import Vehicles from './pages/Vehicles';
 import classes from './App.module.css';
-import StarWars from '../src/assets/StarWarsPoster.png'
-import StarWars2 from '../src/assets/StarWarsposter2.png'
-import StarWars3 from '../src/assets/StarWarsPoster3.png'
-import Logo from '../src/assets/logo.svg'
+import StarWars from '../src/assets/StarWarsPoster.png';
+import StarWars2 from '../src/assets/StarWarsposter2.png';
+import StarWars3 from '../src/assets/StarWarsPoster3.png';
+import Logo from '../src/assets/logo.svg';
 import AddFavorites from './components/AddFavorites';
-
-
-
-
 
 function App() {
   const [showFavorites, setShowFavorites] = useState(false);
   const intervalTime = 3000;
-  const [currentImage,setCurrentImage] = useState(StarWars);
+  const [currentImage, setCurrentImage] = useState(StarWars);
 
-   useEffect(() => {
-      const images = [StarWars, StarWars2, StarWars3];
+  useEffect(() => {
+    const images = [StarWars, StarWars2, StarWars3];
 
     const changeImage = () => {
       const currentIndex = images.indexOf(currentImage);
@@ -30,7 +26,7 @@ function App() {
       setCurrentImage(images[nextIndex]);
     };
 
-     const intervalId = setInterval(changeImage, intervalTime);
+    const intervalId = setInterval(changeImage, intervalTime);
 
     return () => clearInterval(intervalId);
   }, [currentImage, intervalTime]);
@@ -39,57 +35,48 @@ function App() {
     setShowFavorites(true);
   };
 
-   const handleLogo = () => {
+  const handleLogo = () => {
     window.location.href = 'app.js';
   };
 
-
-
   return (
     <Router>
-    <React.Fragment>
-      <section>
-        <header>
-          <div className={classes.navbar}>
-            <img src={Logo} alt='Star wars logo' onClick={handleLogo}></img>
-            <div className={classes.navlinks}>
-              <div className={classes.clickableLinks}>
-              <Link to="/films"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M18 4l2 4h-3l-2-4h-2l2 4h-3l-2-4H8l2 4H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4z"></path></svg>Films</Link>
-             <Link to="/people"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><circle cx="10" cy="8" r="4"></circle><path d="M10.35 14.01C7.62 13.91 2 15.27 2 18v2h9.54c-2.47-2.76-1.23-5.89-1.19-5.99zM19.43 18.02c.36-.59.57-1.28.57-2.02 0-2.21-1.79-4-4-4s-4 1.79-4 4 1.79 4 4 4c.74 0 1.43-.22 2.02-.57L20.59 22 22 20.59l-2.57-2.57zM16 18c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"></path></svg>Characters</Link>
-             <Link to="/starships"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M480 32s-23.4-.7-59.3 27.1C404 72 244.9 186.8 244.9 186.8l-168.8-4.2L32 222l109.8 55.2-8.6 10.8-87.9.1-7.2 40.5 63.1 48.7-26.6 59.8 60-26.4 48.7 63.1 40.5-7.2.1-87.8 10.9-8.5L290.1 480l39.3-44.1-4.2-168.7S440.1 108.2 453 91.6C480.7 55.5 480 32 480 32z"></path></svg>Spaceships</Link>
-             <Link to="/vehicles" ><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M18.92 6.01C18.72 5.42 18.16 5 17.5 5h-11c-.66 0-1.21.42-1.42 1.01L3 12v8c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h12v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-8l-2.08-5.99zM7.5 16c-.83 0-1.5-.67-1.5-1.5S6.67 13 7.5 13s1.5.67 1.5 1.5S8.33 16 7.5 16zm9 0c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zM5.81 10l1.04-3h10.29l1.04 3H5.81z"></path></svg>Vehicles</Link></div>
-             </div>
-
-          <button onClick={handleShowFavorites}>Add Favorites</button>
-         </div>
-        </header>
-      </section>
-      <Switch>
-          <Route path="/films">
-            <Films />
-          </Route>
-          <Route path="/people">
-            <Characters />
-          </Route>
-          <Route path="/starships">
-            <Starships />
-          </Route>
-          <Route path="/vehicles">
-            <Vehicles />
-          </Route>
-        </Switch>
-
-      {showFavorites ? (
-        <AddFavorites setShowFavorites={setShowFavorites} />
-      ) : (
-
+      <React.Fragment>
         <section>
-          <div className={classes['main-image']}>
-            <img src={currentImage} alt='Star wars poster' />
-          </div>
+          <header>
+            <div className={classes.navbar}>
+              <img src={Logo} alt='Star wars logo' onClick={handleLogo}></img>
+              <div className={classes.navlinks}>
+                <div className={classes.clickableLinks}>
+                  <Link to="/films">Films</Link>
+                  <Link to="/people" >Characters</Link>
+                  <Link to="/starships">Spaceships</Link>
+                  <Link to="/vehicles">Vehicles</Link>
+                </div>
+              </div>
+              <button onClick={handleShowFavorites}>Add Favorites</button>
+            </div>
+          </header>
         </section>
-      )}
-    </React.Fragment>
+        <nav>
+          <Routes>
+            <Route path="/films" element={<Films />} />
+            <Route path="/people" element={<Characters />} />
+            <Route path="/starships" element={<Starships />} />
+            <Route path="/vehicles" element={<Vehicles />} />
+          </Routes>
+        </nav>
+
+        {showFavorites ? (
+          <AddFavorites setShowFavorites={setShowFavorites} />
+        ) : (
+          <section>
+            <div className={classes['main-image']}>
+              <img src={currentImage} alt='Star wars poster' />
+            </div>
+          </section>
+        )}
+      </React.Fragment>
     </Router>
   );
 }
--- supreme-swapi-api-project/src/pages/Characters.js
+++ supreme-swapi-api-project/src/pages/Characters.js
@@ -1,14 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import classes from './Characters.module.css';
+
+const CharacterDetails = ({ data }) => {
+  const characterData = [
+    { label: 'Name', value: data.name },
+    { label: 'Gender', value: data.gender },
+    { label: 'Birth Year', value: data.birth_year },
+    { label: 'Height', value: data.eye_color },
+    { label: 'Eye Color', value: data.height }
+
+  ];
+
+  const characterList = characterData.map((item) => (
+    <div className={classes.detailItem} key={item.label}>
+      <span className={classes.label}>{item.label}:</span>
+      <span className={classes.value}>{item.value}</span>
+    </div>
+  ));
+
+  return (
+    <div className={classes.CharactersContainer}>
+      {characterList}
+    </div>
+  );
+};
 
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchCharacters = async () => {
       try {
         const response = await axios.get('https://swapi.dev/api/people/');
         setCharacters(response.data.results);
+        setIsLoading(false);
       } catch (error) {
         console.error('Error fetching characters:', error);
       }
@@ -18,13 +45,21 @@ const Characters = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Star Wars Characters</h1>
-      <ul>
-        {characters.map((character) => (
-          <li key={character.name}>{character.name}</li>
-        ))}
-      </ul>
+    <div className={classes.CharactersContainer}>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <h1>Star Wars Characters</h1>
+          <ul>
+            {characters.map((character) => (
+              <li key={character.name}>
+                <CharacterDetails data={character} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
