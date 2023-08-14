b8d7fed84eb4cfbb4af01b921613ebace63673f9
makoma mabilo <makoma44@gmail.com>
2023-08-14 22:21:44 +0800

    added films page

---
+++ supreme-swapi-api-project/src/pages/Films.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import classes from './Films.module.css';
+
+const FilmsDetails = ({ data }) => {
+  const filmsData = [
+    { label: 'Title', value: data.title },
+    { label: 'Director', value: data.director },
+    { label: 'Release Date', value: data.release_date },
+    { label: 'Characters', value: data.characters }
+
+
+  ];
+
+  const FilmsList = filmsData.map((item) => (
+    <div className={classes.detailItem} key={item.label}>
+      <span className={classes.label}>{item.label}:</span>
+      <span className={classes.value}>{item.value}</span>
+    </div>
+  ));
+
+  return (
+    <div className={classes.FilmsContainer}>
+      {FilmsList}
+    </div>
+  );
+};
+
+const Films = () => {
+  const [films, setFilms] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchFilms = async () => {
+      try {
+        const response = await axios.get('https://swapi.dev/api/films/');
+        setFilms(response.data.results);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching characters:', error);
+      }
+    };
+
+    fetchFilms();
+  }, []);
+
+  return (
+    <div className={classes.FilmsContainer}>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <h1>Star Wars Characters</h1>
+          <ul>
+            {films.map((film) => (
+              <li key={film.title}>
+                <FilmsDetails data={film} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Films;
