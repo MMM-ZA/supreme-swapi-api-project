1aaceae4ad396c6c43e23c44f6232386dfea8103
makoma mabilo <makoma44@gmail.com>
2023-08-26 23:32:36 +0800

    editng starships.js

---
+++ supreme-swapi-api-project/src/pages/Starships.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+const Starships = () => {
+
+  useEffect(() => {
+    const fetchStarships = async () => {
+
+
+
+    }
+
+
+
+
+
+
+   }[],)
+
+
+};
+
+export default Starships;
