69d4e769bdc89b108175ac17c4669ccc917dee9a
makoma mabilo <makoma44@gmail.com>
2024-07-31 15:08:13 +0800

    debugging firebase config

--- supreme-swapi-api-project/src/Firebase/firebase.js
+++ supreme-swapi-api-project/src/Firebase/firebase.js
@@ -2,11 +2,11 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 import firebaseConfig from './firebaseConfig';
 
-
-// Initializing Firebase
+console.log("Firebase Config (before init):", firebaseConfig); 
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+console.log("App Initialized:", app); 
+console.log("App Options:", app.options); 
+const database = getDatabase(app); 
 
-export { database };
-
+export { database }; 
 
